9faf8df5a6b37347f205232fecdf1d2e7e0da67d
Drew Tullos <tullos.andrew@gmail.com>
2024-04-07 18:55:43 -0500

    working post request client

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const cors = require("cors");
 const fs = require("fs");
 const OpenAI = require("openai").default;
 const pdfService = require("./service/pdf-service");
 require("dotenv").config({ path: ".env.local" });
 const app = express();
-const PORT = process.env.PORT || 3001;
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 });
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
+
+// Form Submit Action
 app.post("/submit-form", async (req, res) => {
 	const { goals, height, age, gender } = req.body;
 	const formData = { goals, height, age, gender };
@@ -59,6 +63,5 @@ app.post("/submit-form", async (req, res) => {
 		fs.createReadStream("TrAIner-workout.pdf").pipe(res);
 	});
 });
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+
+module.exports.handler = serverless(app);
